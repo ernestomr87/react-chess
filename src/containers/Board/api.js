15dd4d05b9fffea494b5d02b4634d0553327d7d2
Ernesto Miguel Rodriguez Rodriguez <ernestomr87@gmail.com>
2018-09-19 14:56:56 -0500

    Update

--- src/containers/Board/api.js
+++ src/containers/Board/api.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export function game(fen, move) {
   return axios.get(
-    `http://api.underwaterchess.com/game?fen=${fen}&move=${move}`,
+    `https://api.underwaterchess.com/game?fen=${fen}&move=${move}`,
   );
 }
